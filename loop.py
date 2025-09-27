c032b36b9e9ceb7e47719eb069fdf9fe29899dca
FahadHossainBabor <fahadhossainbabor.cse.ruet@gmail.com>
2025-09-27 09:21:41 +0600

    first commit

---
+++ loop.py
@@ -0,0 +1,7 @@
+magicians = ['alice', 'david', 'carolina']
+
+for magician in magicians:
+    print(magician,end=' ')
+    print(f"\n {magician.title()}, that was a great trick!")
+
+print("\nThank you, everyone. That was a great magic show!")
