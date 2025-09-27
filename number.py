c33357831bbfc5f77c3ebbd80b53ca0613d1102a
FahadHossainBabor <fahadhossainbabor.cse.ruet@gmail.com>
2025-09-27 10:40:09 +0600

    numbers

---
+++ number.py
@@ -0,0 +1,20 @@
+print(2+3)  #sum
+print(69-9) #sub
+print(2*3)  #mul
+print(5/2)  #div
+print(3//2) #integer division
+print (5%2) #Modulus
+print(3**2) #exponent
+
+universe_age = 69_000_000_000
+print(universe_age)
+
+#multiple assignment
+x,y,z = 1,2,3
+
+print(x)
+print(y)
+print(z)
+
+#no keyword for constant -> ALL CAPITAL -> TREATED AS MAX
+MAX_CONNECTIONS = 5000
