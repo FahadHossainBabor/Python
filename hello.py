69b81f75cd68b1cb5e2dff9676c5d0a67a6e3d8b
FahadHossainBabor <fahadhossainbabor.cse.ruet@gmail.com>
2025-09-27 09:29:34 +0600

    learing started

--- hello.py
+++ hello.py
@@ -1,29 +1,3 @@
-# print("Hello world")
+#This program prints 'Hello world to the console'
 
-# message = 'Hello python world'
-# print(message)
-
-# message = 'Hello I am Fahad Hossain Babor'
-# print(message)
-
-# greeting_message = 'Hello nigga'
-# print(greeting_message)
-
-# name ='fahad hossain'
-# print(name.title())
-
-# name = 'Fahad Hossain'
-# name = name.upper()
-# name = name.lower()
-# print(name)
-
-# first_name = 'Fahad Hossain'
-# last_name = 'Babor'
-
-# full_name = f'{first_name} {last_name}'
-# print(full_name)
-
-msg = "     marbo ekhane lash porbe soshane     fatakesto."
-# print(msg.strip()) #pura string er ager picher shob whitespace shorai dibe
-# print(msg.rstrip()) #last er ta shudu shorabe
-print(msg.lstrip())
+print("Hello world")
--- loop.py
+++
@@ -1,7 +0,0 @@
-magicians = ['alice', 'david', 'carolina']
-
-for magician in magicians:
-    print(magician,end=' ')
-    print(f"\n {magician.title()}, that was a great trick!")
-
-print("\nThank you, everyone. That was a great magic show!")
