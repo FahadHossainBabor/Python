850898256a0aabb6edb819c79963ac9707e58065
FahadHossainBabor <fahadhossainbabor.cse.ruet@gmail.com>
2025-09-27 09:46:39 +0600

    variable

---
+++ variable.py
@@ -0,0 +1,47 @@
+#variable
+
+#the variable 'message' stores a value
+#every variable is associated with a value 
+message = 'Hello python world'
+print(message)
+
+
+#updating the variable
+#this operation will change the previous value of the variable 'message'
+message = 'I am Fahad Hossain Babor'
+print(message)
+
+#namig of variable
+#contain only letters,numbers and underscore
+#start with a letter or an underscore
+#e.g: message_1 , but not 1_message
+
+message_1 = 'Never gonna give you up'
+print(message_1)
+
+#spaces are not allowed in variable name 
+message_2 = 'Never gonna let you down' #this is allowed
+# message 2 ; this is not allowed 
+
+#python keyword and functions are not allowed
+
+#Rule of thumb:
+#variable name should be descriptive
+
+#exercise : 2.1
+my_message = '''
+Never gonna give you up,
+Never gonna let you down,
+Never gonna roll around and dessert you.
+Never gonna make you cry,never gonna say good bye and dessert you.
+'''
+
+print(my_message)
+
+# exercise:2.2
+final_message = 'Why are you gay?'
+print(final_message)
+
+final_message = 'Who said I am gay?'
+print(final_message)
+
